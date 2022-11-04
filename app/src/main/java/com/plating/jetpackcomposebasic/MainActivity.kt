0cb667dc238faf2f08cb6374994d4e2a7c30dbe1
minhbka <minhbka@gmail.com>
2022-11-04 13:28:06 +0900

    Create columns and rows

--- app/src/main/java/com/plating/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/plating/jetpackcomposebasic/MainActivity.kt
@@ -3,9 +3,8 @@ package com.plating.jetpackcomposebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -31,18 +30,42 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Composable
+private fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+){
 
+    Column(modifier) {
+        for (name in names){
+            Greeting(name)
+        }
+    }
+
+}
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row (modifier = Modifier.padding(24.dp)){
+            Column(modifier = Modifier
+                .weight(1f)) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = { /*TODO*/ }) {
+                Text(text = "Show more")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true, name="Text preview")
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     JetpackComposeBasicTheme {
-        Greeting("Android")
+       MyApp()
     }
 }
