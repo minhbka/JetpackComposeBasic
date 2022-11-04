d89ac051dab92cd48a5257d90a04e300b5137eee
minhbka <minhbka@gmail.com>
2022-11-04 12:04:43 +0900

    tweaking the ui

--- app/src/main/java/com/plating/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/plating/jetpackcomposebasic/MainActivity.kt
@@ -3,13 +3,16 @@ package com.plating.jetpackcomposebasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.plating.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +34,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 @Preview(showBackground = true, name="Text preview")
