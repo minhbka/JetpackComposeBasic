4d1879f528129dad41a0d580870cb286a43e2f59
minhbka <minhbka@gmail.com>
2022-11-04 14:40:18 +0900

    styling and theming

--- app/src/main/java/com/plating/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/plating/jetpackcomposebasic/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.plating.jetpackcomposebasic
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -18,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.plating.jetpackcomposebasic.ui.theme.JetpackComposeBasicTheme
@@ -107,7 +109,9 @@ fun Greeting(name: String) {
                 .weight(1f)
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
             ElevatedButton(onClick = {
                 expanded = !expanded
@@ -126,6 +130,12 @@ fun DefaultPreview() {
         MyApp(Modifier.fillMaxSize())
     }
 }
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 private fun GreetingsPreview() {
